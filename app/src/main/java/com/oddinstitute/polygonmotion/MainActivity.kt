b9304c5a3bc4571df0269e3a0822aa3539d35420
AmirJahan <amir.jahan@gmail.com>
2021-01-25 05:12:16 -0800

    first

---
+++ app/src/main/java/com/oddinstitute/polygonmotion/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.oddinstitute.polygonmotion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
